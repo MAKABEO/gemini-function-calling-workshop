43bcfa4644b8e69b9d4177b026f2d9b909a1ca20
Jesus Antonio VIllarraga Rojas <jesusantoniovillarraga@gmail.com>
2024-11-23 13:22:39 -0500

    Solucion del workshop

---
+++ src/app/api/transaction/route.js
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import db from "@/data/db.json";
+import { extractData } from "@/app/api/genia";
+
+export async function GET(){
+  return NextResponse.json(db);
+}
+
+export async function POST(request) {
+  const formData = await request.formData();
+  console.log(formData);
+  const description = formData.get("description");
+  const picture = formData.get("picture");
+
+  return createTransaction(description, picture);
+}
+
+async function createTransaction(description, picture) {
+  try {
+    const result = await extractData(description, picture);
+
+    if (!result.date) {
+      result.date = new Date().toISOString();
+    }
+    return NextResponse.json({ transaction: result });
+
+  } catch (error) {
+    return new NextResponse(null, {
+      status: 500,
+      statusText: error.message,
+    });
+  }
+}
